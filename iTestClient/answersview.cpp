76fde2c3be68d1674798e1379f92323e46878277
Ramon Gonzalez <github.com/rggarcia>
2015-06-05 22:27:49 +0200

    Changed to defautl the font size of all elements in the client program

--- iTestClient/answersview.cpp
+++ iTestClient/answersview.cpp
@@ -35,7 +35,6 @@ QWidget(parent) {
     ans_text = new QTextBrowser(this);
     ans_text->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
     ans_text->setMaximumSize(16777215, 59);
-    ans_text->setFontPointSize(15);
     hlayout->addWidget(ans_radiobutton);
     hlayout->addWidget(ans_checkbox);
     hlayout->addWidget(ans_text);
